c2c8dd1b65c37ad542717aad0b8a9fa4eb24f523
Raduev <dominator95rrr@gmail.com>
2021-11-19 16:34:11 +0300

    Ibrem mogila

---
+++ models/User.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const { normalizeTypes } = require('express/lib/utils');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  isBlocked: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
