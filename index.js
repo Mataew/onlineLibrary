b14ac467f881c882c517c47418b869b949ab68b5
Ibragim <mih230903@gmail.com>
2021-11-19 16:08:52 +0300

    все ок

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json())
+
+const connect = async () => {
+    try {
+       await mongoose.connect(
+           "mongodb+srv://Ibragim:munaev230903ibra@cluster0.s3uug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+           )
+           console.log("Сервер успешно соединен с MongoDB")
+           app.listen(4500, () => {
+            console.log("Сервер успешно запущен")
+        })
+    } catch (error) {
+        console.log(error.message) 
+    }
+}
+
+connect();
+
